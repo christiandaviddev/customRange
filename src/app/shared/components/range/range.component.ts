77fa9fd5a8f50b056e58b560088be9360bc6ccba
Christian David Garcia G <cdavid.ggarcia@gmail.com>
2020-11-12 22:38:07 +0100

    calculate normal range slider with two points

--- src/app/shared/components/range/range.component.ts
+++ src/app/shared/components/range/range.component.ts
@@ -1,21 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { fromEvent, Subscription } from 'rxjs';
-import { map, takeWhile, tap } from 'rxjs/operators';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { fromEvent, Observable, Subscription } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map, takeWhile, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'ngc-range',
   templateUrl: './range.component.html',
   styleUrls: ['./range.component.scss']
 })
-export class RangeComponent implements OnInit {
+export class RangeComponent implements OnInit, OnDestroy {
+
+
 
   @Input() min = 0;
   @Input() max = 0;
+  @Input() type = '';
 
-  firstRangeSubscription: Subscription = null;
+  rangeSubscriptions: Subscription[] = [];
   clickRange$: any;
-  firstRangeValue: any = '0px';
-  lastRangeValue: any = '50px';
+
+  firstRangePoint: HTMLSpanElement;
+  lastRangePoint: HTMLSpanElement;
+
+  // firstRangeValue: number;
+  // lastRangeValue: number;
 
   isMouseDownEvent: boolean;
   filterValue:number = 0;
@@ -24,24 +31,64 @@ export class RangeComponent implements OnInit {
 
   ngOnInit(): void {
     const body = document.body as HTMLElement;
+    this.rangeSubscriptions.push(this.setRangePointByClick());
+    // this.setValueRangePoint(firtsRange, 0);
+    // this.setValueRangePoint(lastRange, range.offsetWidth);
+  }
+
+  ngOnDestroy(): void {
+   this.rangeSubscriptions.forEach((s: Subscription) => s.unsubscribe());
+  }
+
+  setRangePointByClick(): Subscription {
+    console.log('setRangePointByClick');
     const range = document.getElementById('range') as HTMLDivElement;
-    const firtsRange = document.getElementById('first-range-point') as HTMLDivElement;
-    this.firstRangeSubscription = fromEvent(range, 'mousedown').pipe(
-      map((event: MouseEvent) => {
-        let valueX = event.pageX;
-        valueX = valueX >= range.offsetWidth ? range.offsetWidth : valueX;
-        valueX = valueX < 0 ? 0 : valueX;
-        return valueX;
-      }),
-      tap(data => console.log('data:  ', data, ' range: ', range.offsetWidth, ' firtsRange: ', firtsRange.offsetWidth))
+    this.firstRangePoint = document.getElementById('first-range-point') as HTMLDivElement;
+    this.lastRangePoint = document.getElementById('last-range-point') as HTMLDivElement;
+
+    this.firtsPointValue = 0;
+    this.lastPointValue = range.offsetWidth;
+    return fromEvent(range, 'mousedown')
+      .pipe(
+        debounceTime(180),
+        distinctUntilChanged(),
+        map((event: MouseEvent) => {
+          let valueX = event.pageX;
+          console.log('valueX a', event.pageX);
+          valueX = valueX >= range.offsetWidth ? range.offsetWidth : valueX;
+          valueX = valueX < 0 ? 0 : valueX;
+          console.log('valueX', valueX);
+          return valueX;
+        })
       ).subscribe(data => {
-        if ( data >= 0 && data <= range.offsetWidth ) {
+          console.log('data', data);
           const value = this.max * ((data / 2) / 100);
           this.filterValue = value >= this.max ? this.max : value;
-          firtsRange.style.left = data - Number(firtsRange.offsetWidth) + 'px';
-        }
+          // rangePoint.style.left = data - Number(rangePoint.offsetWidth) + 'px';
+
+          this.checkRangePoints(data, this.firtsPointValue, this.lastPointValue);
     });
   }
+
+  checkRangePoints(newPosition, firtsPosition, lastPosition) {
+    console.log('newPosition, firtsPosition, lastPosition', newPosition, firtsPosition, lastPosition)
+    let restValueLeft = newPosition - firtsPosition;
+    restValueLeft = restValueLeft < 0 ? restValueLeft * -1 : restValueLeft;
+
+    let restValueRight = newPosition - lastPosition;
+    restValueRight = restValueRight < 0 ? restValueRight * -1 : restValueRight;
+
+    console.log('restValueLeft > restValueRight', restValueLeft , restValueRight);
+    if (restValueLeft <= restValueRight) {
+      this.firtsPointValue = newPosition;
+    } else{
+      this.lastPointValue = newPosition;
+    }
+  }
+
+  // setValueRangePoint(rangePoint: HTMLDivElement, value: number) {
+  //   rangePoint.style.left = value - Number(rangePoint.offsetWidth) + 'px';
+  // }
 
   mouseDownEvent(evnt: any, id: string) {
     console.log('mouseDownEvent');
@@ -66,6 +113,28 @@ export class RangeComponent implements OnInit {
     this.isMouseDownEvent = false;
     // this.firstRangeSubscription.unsubscribe();
   }
+
+
+  public get firtsPointValue(): number {
+    const position = this.firstRangePoint.style.left.replace('px', '');
+    console.log('firstRangePoint', position);
+    return Number(position);
+  }
+
+  public set firtsPointValue(v: number) {
+    this.firstRangePoint.style.left = String(v) + 'px';
+  }
+
+  public get lastPointValue(): number {
+    const position = this.lastRangePoint.style.left.replace('px', '');
+    console.log('firstRangePoint', position);
+    return Number(position);
+  }
+
+  public set lastPointValue(v: number) {
+    this.lastRangePoint.style.left = String(v) + 'px';
+  }
+
 
   // mouseDownEvent(evnt: any, id: string) {
   //   const body = document.body as HTMLElement;
